e50b6e7c64679795602792b77c6d5007162b610c
Hashan Kavinda <delpierokavinda@gmail.com>
2024-12-14 15:08:14 +1000

    feat: send query to open ai and getting response

--- src/AppStart.py
+++ src/AppStart.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import openai
 
 import sys
@@ -16,6 +16,20 @@ openai.api_key = config.OPENAI_API_KEY
 def home():
     return render_template("index.html")
 
+@app.route("/query", methods=["POST", "GET"])
+def query():
+    try:
+        userInput = request.json.get("query", "")
+        if not userInput:
+            return jsonify({ "error": "Text value is empty."}), 400
+        
+        response = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = [{
+                "role": "user",
+                "content": userInput
+            }]
+        )
 @app.route("/error")
 def error():
     return render_template("error.html")
